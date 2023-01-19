ea77348f3c2033ab60439957ce903bc5d68c6e98
qsl <qslyuu@gmail.com>
2023-01-19 17:36:54 +0900

    add form group component

---
+++ src/components/molecules/form-group.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Input from "../atoms/input";
+import "./form-group.css";
+
+type FormGroupProps = {
+  label: string;
+  type?: React.HTMLInputTypeAttribute;
+  name?: string;
+  accept?: string;
+  options?: { label: string; value: number | string }[];
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+const FormGroup: React.FC<FormGroupProps> = (props) => {
+  return (
+    <dl className="form-gruop">
+      <dt>
+        <label>{props.label}</label>
+      </dt>
+      <dd>
+        {props.options ? (
+          props.options.map((option) => (
+            <>
+              <input
+                name={props.name}
+                type={props.type}
+                value={option.value}
+                onChange={props.onChange}
+              />
+              <label>{option.label}</label>
+            </>
+          ))
+        ) : (
+          <Input
+            type={props.type}
+            name={props.name}
+            accept={props.accept}
+            onChange={props.onChange}
+          />
+        )}
+      </dd>
+    </dl>
+  );
+};
+
+export default FormGroup;
