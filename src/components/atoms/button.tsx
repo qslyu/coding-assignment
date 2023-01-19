f97e3efe2267c115f9b0324fa4f20a6d93c134e3
qsl <qslyuu@gmail.com>
2023-01-19 10:41:18 +0900

    add onClick prop to Button component

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./button.css";
 
 type ButtonProps = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   variant: "contained" | "outlined";
   children: React.ReactNode;
 };
 
 const Button: React.FC<ButtonProps> = (props) => {
   return (
-    <button className={`button ${props.variant}`}>{props.children}</button>
+    <button onClick={props.onClick} className={`button ${props.variant}`}>
+      {props.children}
+    </button>
   );
 };
 
