bfcb76adf27064eda6b5060ef1cc12b5ff816c76
qsl <qslyuu@gmail.com>
2023-01-19 10:01:53 +0900

    add header

---
+++ src/components/organisms/header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import NavigationButton from "../atoms/navigation-button";
+import NavigationLink from "../atoms/navigation-link";
+import "./header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="container">
+        <nav className="main-nav">
+          <ul>
+            <li>
+              <NavigationLink to="/">ホーム</NavigationLink>
+            </li>
+            <li>
+              <NavigationLink to="/profile">プロフィール</NavigationLink>
+            </li>
+          </ul>
+        </nav>
+        <ul className="auth-container">
+          <li>
+            <NavigationButton to="/login" variant="outlined">
+              ログイン
+            </NavigationButton>
+          </li>
+          <li>
+            <NavigationButton to="/register" variant="contained">
+              新規登録
+            </NavigationButton>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/templates/page-template.tsx
+++ src/components/templates/page-template.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../organisms/header";
 
 const PageTemplate: React.FC = () => {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
