68cca69c9ddcaae649730973f270109ebc837380
qsl <qslyuu@gmail.com>
2023-01-19 17:38:26 +0900

    wrap outlet component in main tag

--- src/components/templates/page-template.tsx
+++ src/components/templates/page-template.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../organisms/header";
+import "./page-template.css";
 
 const PageTemplate: React.FC = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };
