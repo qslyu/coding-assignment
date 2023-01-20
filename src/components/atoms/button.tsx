b38406562d4d87478df674cb5c4408f9bb0c98e4
qsl <qslyuu@gmail.com>
2023-01-20 23:41:24 +0900

    add disabled prop to button component

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -5,11 +5,16 @@ type ButtonProps = {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   variant: "contained" | "outlined";
   children: React.ReactNode;
+  disabled?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = (props) => {
   return (
-    <button onClick={props.onClick} className={`button ${props.variant}`}>
+    <button
+      onClick={props.onClick}
+      className={`button ${props.variant} ${props.disabled && "disabled"}`}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   );
