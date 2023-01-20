965d90eae6c2168ab1960f7de64bd07440b0e93d
qsl <qslyuu@gmail.com>
2023-01-20 23:43:07 +0900

    add props to input component

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -4,19 +4,24 @@ import "./input.css";
 type InputProps = {
   name?: string;
   type?: React.HTMLInputTypeAttribute;
-  value?: string;
+  value?: string | number;
   accept?: string;
+  min?: string | number;
+  max?: string | number;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  error?: boolean;
 };
 
 const Input: React.FC<InputProps> = (props) => {
   return (
     <input
-      className="input"
+      className={`input ${props.error && "input-error"}`}
       name={props.name}
       type={props.type}
       value={props.value}
       accept={props.accept}
+      min={props.min}
+      max={props.max}
       onChange={props.onChange}
     />
   );
