0797f917538cada3bf8b54d2881973853ee25781
qsl <qslyuu@gmail.com>
2023-01-22 13:32:10 +0900

    changed behavior for unauthenticated users

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -7,36 +7,36 @@ import { useUser } from "../components/templates/page-template";
 const Profile: React.FC = () => {
   const navigate = useNavigate();
   const { user, loadingUser, userData } = useUser();
+  const isLoggedIn = !(!loadingUser && !user);
 
   useEffect(() => {
-    if (!loadingUser && !user) {
-      navigate("/login");
-    }
-    if (
-      userData &&
-      (!userData.avatar ||
-        !userData.userName ||
-        !userData.dateOfBirth ||
-        !userData.gender)
-    ) {
+    if (isLoggedIn && !userData) {
       navigate("/signup/create-profile");
     }
-  }, [user, loadingUser, userData]);
+  }, []);
 
   return (
     <Card>
-      <Avatar
-        size="medium"
-        src={
-          userData ? userData.avatar : import.meta.env.VITE_DEFAULT_AVATAR_SRC
-        }
-      />
-      <h3>{userData?.userName}</h3>
-      <ul>
-        <li>メールアドレス: {user && user.email}</li>
-        <li>生年月日: {userData?.dateOfBirth}</li>
-        <li>性別: {userData?.gender}</li>
-      </ul>
+      {isLoggedIn ? (
+        <>
+          <Avatar
+            size="medium"
+            src={
+              userData
+                ? userData.avatar
+                : import.meta.env.VITE_DEFAULT_AVATAR_SRC
+            }
+          />
+          <h3>{userData?.userName}</h3>
+          <ul>
+            <li>メールアドレス: {user && user.email}</li>
+            <li>生年月日: {userData?.dateOfBirth}</li>
+            <li>性別: {userData?.gender}</li>
+          </ul>
+        </>
+      ) : (
+        <h1>プロフィールを確認するにはログインしてください</h1>
+      )}
     </Card>
   );
 };
